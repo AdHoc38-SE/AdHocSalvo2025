b6d8c18eca9e566e80e7f30326e49be5a36e63fc
AdHoc38-SE <marccarlo433@gmail.com>
2025-02-20 16:14:50 -0500

    Create ship.py

---
+++ ship.py
@@ -0,0 +1,113 @@
+"""
+se3250lib Version 0.1.0
+
+Copyright (c) 2024 FEONA MAE L. JOHNSON
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+ship_inv = {
+    "LCS-Freedom": {
+        "type": 'LCS-Freedom',
+        "classtype": 'LCS',
+        "op": 2.4,
+        "dp": 2.1,
+        "sp": 1.15,
+        "age": 8,
+        "training": 0.85,
+    },
+    "LCS-Independence": {
+        "type": 'LCS-Independence',
+        "classtype": 'LCS',
+        "op": 3.2,
+        "dp": 11,
+        "sp": 1.27,
+        "age": 6,
+        "training": 0.88,
+    },
+    "DDG-Arleigh-Burke": {
+        "type": 'DDG-Arleigh-Burke',
+        "classtype": 'DDG',
+        "op": 10.4,
+        "dp": 9,
+        "sp": 1.54,
+        "age": 12,
+        "training": 0.92,
+    },
+    "CG-Ticonderoga": {
+        "type": 'CG-Ticonderoga',
+        "classtype": 'CG',
+        "op": 13.8,
+        "dp": 6.8,
+        "sp": 1.73,
+        "age": 18,
+        "training": 0.88,
+    },
+    "Destroyer-055D-Renhai": {
+        "type": 'Destroyer-055D-Renhai',
+        "classtype": 'Destroyer',
+        "op": 11.2,
+        "dp": 2.4,
+        "sp": 1.8,
+        "age": 4,
+        "training": 0.92,
+    },
+    "Destroyer-052D-Luyang-III": {
+        "type": 'Destroyer-052D-Luyang-III',
+        "classtype": 'Destroyer',
+        "op": 6.4,
+        "dp": 2.4,
+        "sp": 1.57,
+        "age": 8,
+        "training": 0.9,
+    },
+    "Frigate-054A-Jiangkai-II": {
+        "type": 'Frigate-054A-Jiangkai-II',
+        "classtype": 'Frigate',
+        "op": 0.8,
+        "dp": 0.32,
+        "sp": 1.34,
+        "age": 10,
+        "training": 0.87,
+    },
+    "Frigate-053H3-Jiangwei-II": {
+        "type": 'Frigate-053H3-Jiangwei-II',
+        "classtype": 'Frigate',
+        "op": 0.8,
+        "dp": 0.8,
+        "sp": 1.12,
+        "age": 14,
+        "training": 0.84,
+    },
+    "Corvette-056A-Jiangdao": {
+        "type": 'Corvette-056A-Jiangdao',
+        "classtype": 'Corvette',
+        "op": 0.4,
+        "dp": 0.8,
+        "sp": 0.9,
+        "age": 6,
+        "training": 0.88,
+    },
+        "Test-1": {
+        "type": 'Test-1',
+        "classtype": 'XX',
+        "op": 4,
+        "dp": 4,
+        "sp": 2,
+        "age": 0,
+        "training": 1,
+    },
+        "Test-2": {
+        "type": 'Test-2',
+        "classtype": 'XX',
+        "op": 4,
+        "dp": 4,
+        "sp": 2,
+        "age": 0,
+        "training": 1,
+    }
+}
