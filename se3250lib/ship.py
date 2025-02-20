2b6248b79c21516bb92c3ba381f3d5b2db957a01
AdHoc38-SE <marccarlo433@gmail.com>
2025-02-20 17:43:56 -0500

    Update ship.py

--- se3250lib/ship.py
+++ se3250lib/ship.py
@@ -8,6 +8,18 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    ''' A ship carrying anti-ship cruise missiles.
+
+    Attributes:
+        * type (str): the type of ship, for labeling purposes only.
+        * op (int): the number of anti-ship cruise missiles the ship can fire in one salvo.
+        * dp (int): the number of SAM the ship can fire in one salvo against incoming missiles.
+        * sp (float): initial staying power in missile hits.
+        * hp (float): hit points remaining.
+        * status (fraction): fraction of its staying power remaining. 1 is intact, 0 is OOA.
+    '''
+
 """
 
 ship_inv = {
@@ -47,67 +59,4 @@ ship_inv = {
         "age": 18,
         "training": 0.88,
     },
-    "Destroyer-055D-Renhai": {
-        "type": 'Destroyer-055D-Renhai',
-        "classtype": 'Destroyer',
-        "op": 11.2,
-        "dp": 2.4,
-        "sp": 1.8,
-        "age": 4,
-        "training": 0.92,
-    },
-    "Destroyer-052D-Luyang-III": {
-        "type": 'Destroyer-052D-Luyang-III',
-        "classtype": 'Destroyer',
-        "op": 6.4,
-        "dp": 2.4,
-        "sp": 1.57,
-        "age": 8,
-        "training": 0.9,
-    },
-    "Frigate-054A-Jiangkai-II": {
-        "type": 'Frigate-054A-Jiangkai-II',
-        "classtype": 'Frigate',
-        "op": 0.8,
-        "dp": 0.32,
-        "sp": 1.34,
-        "age": 10,
-        "training": 0.87,
-    },
-    "Frigate-053H3-Jiangwei-II": {
-        "type": 'Frigate-053H3-Jiangwei-II',
-        "classtype": 'Frigate',
-        "op": 0.8,
-        "dp": 0.8,
-        "sp": 1.12,
-        "age": 14,
-        "training": 0.84,
-    },
-    "Corvette-056A-Jiangdao": {
-        "type": 'Corvette-056A-Jiangdao',
-        "classtype": 'Corvette',
-        "op": 0.4,
-        "dp": 0.8,
-        "sp": 0.9,
-        "age": 6,
-        "training": 0.88,
-    },
-        "Test-1": {
-        "type": 'Test-1',
-        "classtype": 'XX',
-        "op": 4,
-        "dp": 4,
-        "sp": 2,
-        "age": 0,
-        "training": 1,
-    },
-        "Test-2": {
-        "type": 'Test-2',
-        "classtype": 'XX',
-        "op": 4,
-        "dp": 4,
-        "sp": 2,
-        "age": 0,
-        "training": 1,
-    }
 }
