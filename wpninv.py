2d9f77ae311a803d24bbe1b7e0a9d40e4efea383
AdHoc38-SE <marccarlo433@gmail.com>
2025-02-20 16:15:42 -0500

    Create wpninv.py

---
+++ wpninv.py
@@ -0,0 +1,118 @@
+"""
+se3250lib Version 0.1.0
+
+Copyright (c) 2024 FEONA MAE L. JOHNSON
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+wpn_inv = {
+    "AGM-114L": {
+        "type": 'AGM-114L',
+        "classtype": 'AGM',
+        "launch_reliability": 0.92,
+        "ascm_to_hit": 0.72,
+        "sam_to_hit": 0.58,
+    },
+    "AGM-84E": {
+        "type": 'AGM-84E',
+        "classtype": 'AGM',
+        "launch_reliability": 0.88,
+        "ascm_to_hit": 0.65,
+        "sam_to_hit": 0.62,
+    },
+    "BGM-109": {
+        "type": 'BGM-109',
+        "classtype": 'BGM',
+        "launch_reliability": 0.94,
+        "ascm_to_hit": 0.78,
+        "sam_to_hit": 0.51,
+    },
+    "RIM-116": {
+        "type": 'RIM-116',
+        "classtype": 'RIM',
+        "launch_reliability": 0.85,
+        "ascm_to_hit": 0.48,
+        "sam_to_hit": 0.75,
+    },
+    "RIM-66M": {
+        "type": 'RIM-66M',
+        "classtype": 'RIM',
+        "launch_reliability": 0.85,
+        "ascm_to_hit": 0.48,
+        "sam_to_hit": 0.75,
+    },
+    "RGM-84": {
+        "type": 'RGM-84',
+        "classtype": 'RGM',
+        "launch_reliability": 0.91,
+        "ascm_to_hit": 0.69,
+        "sam_to_hit": 0.63,
+    },
+    "RGM-184A": {
+        "type": 'RGM-184A',
+        "classtype": 'RGM',
+        "launch_reliability": 0.93,
+        "ascm_to_hit": 0.74,
+        "sam_to_hit": 0.59,
+    },
+    "FL-300N": {
+        "type": 'FL-300N',
+        "classtype": 'FL',
+        "launch_reliability": 0.87,
+        "ascm_to_hit": 0.66,
+        "sam_to_hit": 0.71,
+    },
+    "HQ-10": {
+        "type": 'HQ-10',
+        "classtype": 'HQ',
+        "launch_reliability": 0.78,
+        "ascm_to_hit": 0.61,
+        "sam_to_hit": 0.67,
+    },
+    "HQ-16": {
+        "type": 'HQ-16',
+        "classtype": 'HQ',
+        "launch_reliability": 0.89,
+        "ascm_to_hit": 0.57,
+        "sam_to_hit": 0.73,
+    },
+    "YJ-18": {
+        "type": 'YJ-18',
+        "classtype": 'YJ',
+        "launch_reliability": 0.92,
+        "ascm_to_hit": 0.55,
+        "sam_to_hit": 0.68,
+    },
+    "YJ-12": {
+        "type": 'YJ-12',
+        "classtype": 'YJ',
+        "launch_reliability": 0.86,
+        "ascm_to_hit": 0.35,
+        "sam_to_hit": 0.68,
+    },
+    "YJ-83": {
+        "type": 'YJ-83',
+        "classtype": 'YJ',
+        "launch_reliability": 0.88,
+        "ascm_to_hit": 0.55,
+        "sam_to_hit": 0.78,
+    },
+    "Test-1": {
+        "type": 'Test-1',
+        "classtype": 'XX',
+        "launch_reliability": 1,
+        "ascm_to_hit": 0.61,
+        "sam_to_hit": 0.35,
+    },
+    "Test-2": {
+        "type": 'Test-2',
+        "classtype": 'XX',
+        "launch_reliability": 1,
+        "ascm_to_hit": 0.61,
+        "sam_to_hit": 0.35,
+    }    
+}
